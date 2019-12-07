578cbcd71fd301ab94206c7eaf84bcc7a0b5fd7f
lytves <22356770+lytves@users.noreply.github.com>
2019-12-07 17:37:10 +0100

    added last mentioned ID config & file

--- twitterlist/config.py
+++ twitterlist/config.py
@@ -1,6 +1,7 @@
 # Telegram
 TG_BOT_TOKEN = "..."
-TG_YOUR_TELEGRAM_PRIVATE_CHAT_ID = ...
+TG_YOUR_TELEGRAM_PRIVATE_LIST_MSG_CHAT_ID = ...
+TG_YOUR_TELEGRAM_PRIVATE_MENTIONED_MSG_CHAT_ID = ...
 
 # Twitter
 TW_CONSUMER_KEY = "..."
@@ -10,4 +11,5 @@ TW_TOKEN_SECRET = "..."
 
 TW_LIST_OWNER_SCREEN_NAME = "..."
 TW_LIST_SLUG = "..."
-TW_LAST_TWIT_ID_FILENAME = '/tw_last_twit_id.txt'
+TW_LAST_LIST_TWIT_ID_FILENAME = '/tw_last_list_twit_id.txt'
+TW_LAST_MENTIONED_TWIT_ID_FILENAME = '/tw_last_mentioned_twit_id.txt'
